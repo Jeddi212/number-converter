28cda88b330d72798a882afb985df77c100105d7
Jeddi212 <jedediahf51@gmail.com>
2022-06-06 19:42:33 +0700

    add some str_to, util, serivce

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod util;
 
+use crate::util::welcome;
 use crate::util::user_input;
 
 fn main() {
-    let mut input: String = String::new();
-    user_input::read(&mut input);
-    println!("Hello, world! {}", &input);
+    let mut user_choose: String = String::new();
+
+    welcome::greet(&mut user_choose);
+    let num = user_input::str_to_i32(&mut user_choose);
+    
+    print!("your number is : {}", num);
 }
